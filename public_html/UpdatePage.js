5de660eab338a93f903e15eb487f744ba7319a22
yasmen24 <yasmensaleh24@gmail.com>
2024-02-07 12:05:54 +0300

    add files

---
+++ public_html/UpdatePage.js
@@ -0,0 +1,11 @@
+
+var btn=document.getElementsByClassName("btn")[0];
+console.log(btn);
+
+
+
+
+btn.addEventListener("click", function() {
+  // Change the URL to the desired page
+  window.location.href = "DesignerHomepage.html";
+});
