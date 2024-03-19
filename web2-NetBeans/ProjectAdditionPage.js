aad0446f6008a5eeef114bf13191cc625de26202
yasmen24 <yasmensaleh24@gmail.com>
2024-03-19 23:40:40 +0300

    add DB

--- web2-NetBeans/ProjectAdditionPage.js
+++
@@ -1,9 +0,0 @@
-var btn=document.getElementById("btn");
-
-
-console.log(btn)
-
-btn.addEventListener("click", function() {
-  // Change the URL to the desired page
-  window.location.href = "DesignerHomepage.html";
-});
